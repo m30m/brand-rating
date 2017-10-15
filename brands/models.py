4bf823bed1937cb6fc1f5ef2cc813592b09091ac
Mohammad Amin Khashkhashi Moghaddam <amin.moghaddamv@gmail.com>
2017-10-15 12:46:48 +0330

    Add time for BrandRating model

--- brands/models.py
+++ brands/models.py
@@ -10,3 +10,4 @@ class BrandRating(models.Model):
     brand = models.ForeignKey(Brand)
     rating = models.SmallIntegerField()
     ip = models.GenericIPAddressField()
+    time = models.DateTimeField(auto_now_add=True, null=True)

7cebd4e2a8043dae2f40b3965a5f6be189d035f8
Mohammad Amin Khashkhashi Moghaddam <amin.moghaddamv@gmail.com>
2017-10-16 21:37:36 +0330

    Add static libs locally

--- brandrating/urls.py
+++ brandrating/urls.py
@@ -22,4 +22,5 @@ from django.conf import settings
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^', include('brands.urls')),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)\
++ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
